9c5b324728affe6fbe802dfdcdb503cbdd297c11
Oybek Sultanov <oibekzzz09@gmail.com>
2024-06-28 20:57:46 +0600

    Реализация комопнента navbar для переулючения между страницами

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import Home from './containers/Home/Home';
+import Contacts from './containers/Contacts/Contacts';
 import Navbar from './components/Navbar/Navbar';
+import About from './containers/About/About';
+import {Route, Routes} from 'react-router-dom';
 
-const App = () => {
+const App = () => (
+  <>
+    <header>
+      <Navbar/>
+    </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+      </Routes>
+  </>
+);
 
-  return (
-    <>
-     <header>
-       <Navbar/>
-     </header>
-    </>
-  )
-};
-
-export default App
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,31 @@
+import { NavLink } from 'react-router-dom';
+import { AppBar, Toolbar, Button} from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledNavLink = styled(NavLink)({
+  textDecoration: 'none',
+  color: 'inherit',
+  '&.active': {
+    color: '#f50057',
+    fontWeight: 'bold',
+  },
+});
+
 const Navbar = () => {
   return (
-    <div>
-      NavBar
-    </div>
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Button color="inherit" component={StyledNavLink} to="/" end>
+          Home
+        </Button>
+        <Button color="inherit" component={StyledNavLink} to="/about">
+          About
+        </Button>
+        <Button color="inherit" component={StyledNavLink} to="/contacts">
+          Contacts
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
